cb81863b62d53fd19b3dd11426d394c97ada2cbf
MariBanti <119875944+MariBanti@users.noreply.github.com>
2023-10-09 00:39:20 +0300

    Add files via upload

---
+++ lab1_1/lab1_1_child.cpp
@@ -0,0 +1,25 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <iostream>
+
+BOOL WINAPI HandlerRoutine(DWORD ctrlType) {
+	switch (ctrlType)
+	{
+	case CTRL_CLOSE_EVENT:
+		std::cout << "CTRL close event" << std::endl;
+		break;
+	case CTRL_C_EVENT:
+		std::cout << "CTRL+c event" << std::endl;
+		break;
+	default:
+		break;
+	}
+	return false;
+}
+
+int main() {
+	while (SetConsoleCtrlHandler(HandlerRoutine, true)) {
+		continue;
+	}
+	return 0;
+}
